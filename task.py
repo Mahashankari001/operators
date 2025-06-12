349d58705a82adb5eba45aecc078457e162e7621
Mahashankari001 <mahareena2000@gmail.com>
2025-06-12 17:37:53 +0530

    operators

---
+++ task.py
@@ -0,0 +1,134 @@
+1)
+	a=6
+	b=7
+	c=8
+	print(c/a,b*a,c**b)
+	
+	#output
+		1.3333333333333333 42 2097152
+
+2)
+	a=6
+	b=7
+	c=8
+	print(c//a,b-a,b%c)
+
+	#output
+		1 1 7
+
+3)
+	a=30
+	a*=2
+	print(a)
+
+	#output
+		60
+
+4)
+	z=4
+	z+=2
+	print(z)
+	
+	#output
+		6
+
+5)
+	x=5
+	x%=2
+	y=10
+	y/=5
+	print(x+y)
+x=6
+
+	#output
+		3.0
+
+6)
+	x=6
+	y=8
+	z=10
+	print(x<=10,y==6,z>y)
+
+	#output
+		True False True
+7)
+	x=6
+	y=8
+	z=10
+	print((y<=x) and (y==8) or (z>y) and (x>=5))
+	
+	#output
+		True
+
+8)
+	x=6
+	y=7
+	print(x&y)
+
+	#output
+		6
+
+9)
+	x=7
+	y=6
+	print(x/y)
+
+	#output
+		7
+
+10)
+	x=6
+	y=8
+	z=7
+	print(x<<2,y/z,x%z,y>>3)
+
+	#output
+		24 15 6 1
+
+11)
+	a=30
+	b=14
+	c=13
+	print(a^b,b&c,a^c,c&a)
+	
+	#output
+		16 12 19 12
+
+12)
+	print("dhanam+reenu","dhanam","reenu")
+
+	#output
+		dhanam+reenu dhanam reenu
+
+13)
+	x=5
+	y=4
+	z=11
+	print(not(z>10) and (x>4))
+
+	#output
+		False
+
+14)
+	x=5
+	y=4
+	z=11
+	print(x<<2,y>>3,z>>3)
+		
+	#output
+		20 0 1
+
+15)
+	x=7
+	y=4
+	z=2
+	print(x-y,z+x,y/z,y%x)
+
+	#output
+		3 9 2.0 4
+
+	
+		
+		
+		
+		
